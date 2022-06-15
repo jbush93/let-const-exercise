95de0658355f32bfd3e8ac38ec1cce8ec15a02b1
Jer <jrod9393@gmail.com>
2022-06-14 19:28:22 -0500

    committing obj enhance exercise for upload to github repo

---
+++ objEnhanceEx.js
@@ -0,0 +1,34 @@
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+  let favoriteNumber = 42;
+  const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+  }
+
+  const instructor = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
+
+const dog = createAnimal('dog','barks','woof');
+console.log(dog.barks())
